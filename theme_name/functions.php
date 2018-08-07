b5e3caa22e428a2d6b5dff85ab4ae87e7527f452
josh581 <josh@deltstl.com>
2018-08-07 12:38:21 -0500

    first commit

---
+++ theme_name/functions.php
@@ -0,0 +1,36 @@
+function redirect_login_page() {
+	$login_page  = home_url('/login/');
+	$page_viewed = basename($_SERVER['REQUEST_URI']);
+
+	if($page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+		wp_redirect($login_page);
+		exit;
+	}
+}
+add_action('init','redirect_login_page');
+
+/* Where to go if a login failed */
+function custom_login_failed() {
+	$login_page  = home_url('/login/');
+	wp_redirect($login_page . '?login=failed');
+	exit;
+}
+add_action('wp_login_failed', 'custom_login_failed');
+
+/* Where to go if any of the fields were empty */
+function verify_user_pass($user, $username, $password) {
+	$login_page  = home_url('/login/');
+	if($username == "" || $password == "") {
+		wp_redirect($login_page . "?login=empty");
+		exit;
+	}
+}
+add_filter('authenticate', 'verify_user_pass', 1, 3);
+
+/* What to do on logout */
+function logout_redirect() {
+	$login_page  = home_url('/login/');
+	wp_redirect($login_page . "?login=false");
+	exit;
+}
+add_action('wp_logout','logout_redirect');
